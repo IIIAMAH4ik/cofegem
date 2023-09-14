d97168a2689321f62a41794094851703396d2f75
IIIAMAH4ik <119852054+IIIAMAH4ik@users.noreply.github.com>
2023-09-14 21:35:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function showLayer(layerId) {
+    // Скрыть все слои
+    const layers = document.querySelectorAll('.layer');
+    layers.forEach(layer => {
+        layer.classList.remove('active-layer');
+    });
+
+    // Показать выбранный слой
+    const selectedLayer = document.getElementById(layerId);
+    if (selectedLayer) {
+        selectedLayer.classList.add('active-layer');
+    }
+}
+
+function showOptions(button) {
+    // Находим родительский элемент (контейнер товара)
+    const product = button.closest('.product');
+
+    // Находим элемент, который будет отображать количество товаров в корзине
+    const itemCountElement = product.querySelector('.item-count');
+
+    // Получаем текущее количество товаров
+    let itemCount = parseInt(itemCountElement.textContent);
+
+    // Находим кнопки "+-" внутри контейнера товара
+    const optionButton1 = product.querySelector('.option-button1');
+    const optionButton2 = product.querySelector('.option-button2');
+
+    if (button === optionButton1 && itemCount > 0) {
+        // Если нажата кнопка "-", уменьшаем количество товаров (но не меньше 0)
+        itemCount = Math.max(itemCount - 1, 0);
+    } else if (button === optionButton2) {
+        // Если нажата кнопка "+", увеличиваем количество товаров
+        itemCount += 1;
+    }
+
+    // Обновляем отображение количества товаров
+    itemCountElement.textContent = itemCount;
+
+    // Показываем или скрываем кнопки "+-" в зависимости от значения itemCount
+    const addButton = product.querySelector('.add-button');
+    
+    if (itemCount === 0) {
+        addButton.style.display = 'inline-block';
+        optionButton1.style.display = 'none';
+        optionButton2.style.display = 'none';
+    } else {
+        addButton.style.display = 'none';
+        optionButton1.style.display = 'inline-block';
+        optionButton2.style.display = 'inline-block';
+    }
+}
